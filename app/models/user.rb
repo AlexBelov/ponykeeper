2a6027cc56be08060ec51cba0d3bce7370fc88d1
Alexander Belov <belovby@gmail.com>
2020-09-10 08:00:50 -0700

    Create users, books and drinks

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_and_belongs_to_many :books
+  has_and_belongs_to_many :drinks
+end
---
+++ db/migrate/20200910145956_create_books_users.rb
@@ -0,0 +1,10 @@
+class CreateBooksUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books_users do |t|
+      t.integer :user_id
+      t.integer :book_id
+
+      t.timestamps
+    end
+  end
+end
