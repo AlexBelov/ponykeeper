8160cad87b8a5f209786309f395ea84c618d3564
Alexander Belov <belovby@gmail.com>
2020-09-10 11:02:30 -0700

    Default url options

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Buhlib
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
     # Rails.application.routes.default_url_options[:host] = 'https://fd190b8aa897.ngrok.io'
+    Rails.application.routes.default_url_options[:host] = 'https://buhlib.herokuapp.com'
   end
 end
