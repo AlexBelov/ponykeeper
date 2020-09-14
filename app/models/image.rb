4933484e44123ee34914ec49c053080d36953118
Alexander Belov <belovby@gmail.com>
2020-09-14 08:50:52 -0700

    Show image in show

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,17 @@ class Image < ApplicationRecord
   def self.random(image_types = entities.values)
     where(entity: image_types).all.sample.try(:url)
   end
+
+  rails_admin do
+    show do
+      field :entity
+      field :url
+      field :image do
+        formatted_value do
+          bindings[:view].tag(:img, { src: bindings[:object].url })
+        end
+      end
+      include_all_fields
+    end
+  end
 end
