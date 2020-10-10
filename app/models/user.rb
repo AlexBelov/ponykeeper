b00e2ac520ba3efba726e9d94f1b7e741480b03a
Alexander Belov <belovby@gmail.com>
2020-10-10 06:00:07 -0700

    Age and groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :admin
+  belongs_to :group
 
   def full_name
     [first_name, last_name].join(' ').strip
@@ -21,5 +22,13 @@ class User < ApplicationRecord
   def self.handle_user(from)
     return true unless from.present?
     User.where(telegram_id: from['id']).first_or_create(first_name: from['first_name'], last_name: from['last_name'], username: from['username'])
+  end
+
+  rails_admin do
+    list do
+      field :id
+      field :group
+      include_all_fields
+    end
   end
 end
